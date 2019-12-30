34058231d0962d8add3c12936222085b853eeccd
Conor McAfee <hello@conormcafee.co.uk>
2019-12-30 23:36:00 +0000

    Refactor of BTTS

--- src/betTypes/btts.js
+++ src/betTypes/btts.js
@@ -4,17 +4,15 @@ export const BTTS = (matches = [], teamId = 0, type = "") => {
   );
 
   if (type === "home") {
+    const homeGames = teamMatches.filter(match => match.homeTeam.id === teamId);
     return `${
-      teamMatches
-        .filter(match => match.homeTeam.id === teamId)
-        .filter(match => match.score.fullTime.homeTeam > 0).length
-    }/${teamMatches.filter(match => match.homeTeam.id === teamId).length}`;
+      homeGames.filter(match => match.score.fullTime.homeTeam > 0).length
+    }/${homeGames.length}`;
   } else if (type === "away") {
+    const awayGames = teamMatches.filter(match => match.awayTeam.id === teamId);
     return `${
-      teamMatches
-        .filter(match => match.awayTeam.id === teamId)
-        .filter(match => match.score.fullTime.awayTeam > 0).length
-    }/${teamMatches.filter(match => match.awayTeam.id === teamId).length}`;
+      awayGames.filter(match => match.score.fullTime.awayTeam > 0).length
+    }/${awayGames.length}`;
   } else if (type === "both") {
     return teamMatches.filter(
       match =>
